38670626a9a280bad29c6f631476fbd9a3f2bc05
devpankaj123 <erpankajkumar953@gmail.com>
2020-04-01 03:41:29 +0530

    React-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import Home from './Component/Home';
 import About from './Component/About'
+import Contact from './Component/Contact'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 export default class App extends Component {
   constructor(props) {
     super(props)
@@ -15,9 +22,32 @@ export default class App extends Component {
   render() {
     const {name} = this.state;
     return (
+
       <React.Fragment>
-        <Home handleNameChange = {this.changeAuthorName} authorName ={name} />
-        <About authorName ={name} />
+        <Router>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/">
+              <Home handleNameChange = {this.changeAuthorName} authorName ={name} />
+            </Route>
+            <Route path="/about">
+               <About authorName ={name} />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </Router>
       </React.Fragment>
     );
   }
---
+++ src/Component/Contact.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+export default class Contact extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+        <p>This is contact Component</p>
+        );
+    }
+}
